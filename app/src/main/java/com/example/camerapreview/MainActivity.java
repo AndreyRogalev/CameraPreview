7cf6eba107ca2d94ea508453e1074ca009aea8aa
AndreyRogalev <rogalevandrey@gmail.com>
2025-05-03 06:59:14 +0000

    to Action

---
+++ app/src/main/java/com/example/camerapreview/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.camerapreview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Установите ваш макет, например:
+        // setContentView(R.layout.activity_main);
+        // TODO: Добавьте сюда логику вашей Activity
+    }
+}
